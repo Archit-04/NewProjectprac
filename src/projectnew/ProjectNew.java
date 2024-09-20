3e3c0a8899c7f37b0b852ff7301f6ee7a07379c5
Archit-04 <architar@sheridancollege.ca>
2024-09-20 09:49:09 -0400

    committing

---
+++ src/projectnew/ProjectNew.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package projectnew;
+
+/**
+ *
+ * @author archi
+ */
+public class ProjectNew {
+
+    /**
+     * @param args the command line arguments
+     */
+    //Hey
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
